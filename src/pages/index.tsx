6f4b423ccdaadec11f243d8c5192c81b1b1302f6
lipohong <lipohong@hotmail.com>
2021-05-25 12:03:51 +0800

    Update scss files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { useEffect }from 'react';
+import { useEffect, Suspense }from 'react';
 import LinerColor from '../models/enum/linerColor';
-import OperationSign from '../components/operationSign';
+const OperationSign = React.lazy(() => import('../components/operationSign'));
 import OperationSignType from '../models/enum/operationSignType';
 
 
-const threshold = 0;
+const threshold = 100;
 
 const HomePage: React.FunctionComponent = () => {
   const handleScrollAndResize = () => {
@@ -30,7 +30,7 @@ const HomePage: React.FunctionComponent = () => {
   }, []);
 
   return (
-    <>
+    <Suspense fallback={<div>loading...</div>}>
       <section className="firstSection">
         <div className="container">
           <section className="mainCover">
@@ -44,32 +44,32 @@ const HomePage: React.FunctionComponent = () => {
           <section className="backgroundCover"></section>
           <section className="operationSignGroup-1">
             <OperationSign type={OperationSignType.addition} size={3} color={LinerColor.flare} rotation={0} zIndex={1} top={4} left={-3} />
-            <OperationSign type={OperationSignType.addition} size={3} color={LinerColor['dark-ocean']} rotation={135} zIndex={2} top={4} left={1} />
+            <OperationSign type={OperationSignType.addition} size={3} color={LinerColor.darkOcean} rotation={135} zIndex={2} top={4} left={1} />
             <OperationSign type={OperationSignType.subtraction} size={3} color={LinerColor.frozen} rotation={90} zIndex={3} top={0.5} left={-2} />
-            <OperationSign type={OperationSignType.division} size={3} color={LinerColor['aqua-marine']} rotation={135} zIndex={4} top={0} left={1} />
+            <OperationSign type={OperationSignType.division} size={3} color={LinerColor.aquaMarine} rotation={135} zIndex={4} top={0} left={1} />
           </section>
           <section className="operationSignGroup-2">
             <OperationSign type={OperationSignType.addition} size={8} color={LinerColor.frozen} rotation={0} zIndex={1} top={0} left={-5} />
-            <OperationSign type={OperationSignType.addition} size={13} color={LinerColor['evening-sunshine']} rotation={0} zIndex={1} top={12} left={-43} />
+            <OperationSign type={OperationSignType.addition} size={13} color={LinerColor.eveningSunshine} rotation={0} zIndex={1} top={12} left={-43} />
             <OperationSign type={OperationSignType.addition} size={10} color={LinerColor.metapolis} rotation={45} zIndex={2} top={28} left={-35} />
             <OperationSign type={OperationSignType.addition} size={5} color={LinerColor.memariani} rotation={0} zIndex={3} top={8} left={-12} />
-            <OperationSign type={OperationSignType.addition} size={8} color={LinerColor['piggy-pink']} rotation={45} zIndex={4} top={15} left={-12} />
-            <OperationSign type={OperationSignType.addition} size={12} color={LinerColor['grade-grey']} rotation={135} zIndex={5} top={30} left={0} />
-            <OperationSign type={OperationSignType.subtraction} size={10} color={LinerColor['rose-water']} rotation={135} zIndex={6} top={18} left={-22} />
+            <OperationSign type={OperationSignType.addition} size={8} color={LinerColor.piggyPink} rotation={45} zIndex={4} top={15} left={-12} />
+            <OperationSign type={OperationSignType.addition} size={12} color={LinerColor.gradeGrey} rotation={135} zIndex={5} top={30} left={0} />
+            <OperationSign type={OperationSignType.subtraction} size={10} color={LinerColor.roseWater} rotation={135} zIndex={6} top={18} left={-22} />
             <OperationSign type={OperationSignType.subtraction} size={15} color={LinerColor.harvey} rotation={45} zIndex={7} top={16} left={15} />
-            <OperationSign type={OperationSignType.subtraction} size={10} color={LinerColor['mango-pulp']} rotation={90} zIndex={8} top={20} left={-2} />
-            <OperationSign type={OperationSignType.division} size={10} color={LinerColor['j-shine']} rotation={135} zIndex={9} top={-5} left={10} />
+            <OperationSign type={OperationSignType.subtraction} size={10} color={LinerColor.mangoPulp} rotation={90} zIndex={8} top={20} left={-2} />
+            <OperationSign type={OperationSignType.division} size={10} color={LinerColor.jShine} rotation={135} zIndex={9} top={-5} left={10} />
             <OperationSign type={OperationSignType.division} size={12} color={LinerColor.neuromancer} rotation={135} zIndex={10} top={35} left={-18} />
-            <OperationSign type={OperationSignType.division} size={15} color={LinerColor['dark-ocean']} rotation={90} zIndex={11} top={-5} left={-32} />
+            <OperationSign type={OperationSignType.division} size={15} color={LinerColor.darkOcean} rotation={90} zIndex={11} top={-5} left={-32} />
           </section>
           <section className="operationSignGroup-3">
             <OperationSign type={OperationSignType.subtraction} size={10} color={LinerColor.amin} rotation={45} zIndex={1} top={-5} left={50} />
             <OperationSign type={OperationSignType.subtraction} size={12} color={LinerColor.yoda} rotation={45} zIndex={1} top={-5} left={65} />
             <OperationSign type={OperationSignType.subtraction} size={17} color={LinerColor.sunrise} rotation={45} zIndex={1} top={0} left={75} />
             <OperationSign type={OperationSignType.subtraction} size={20} color={LinerColor.frozen} rotation={-135} zIndex={1} top={12} left={48} />
-            <OperationSign type={OperationSignType.subtraction} size={13} color={LinerColor['witching-hour']} rotation={45} zIndex={1} top={20} left={66} />
-            <OperationSign type={OperationSignType.subtraction} size={15} color={LinerColor['moonlit-asteroid']} rotation={-135} zIndex={1} top={30} left={50} />
-            <OperationSign type={OperationSignType.subtraction} size={21} color={LinerColor['bloody-mary']} rotation={-135} zIndex={1} top={30} left={70} />
+            <OperationSign type={OperationSignType.subtraction} size={13} color={LinerColor.witchingHour} rotation={45} zIndex={1} top={20} left={66} />
+            <OperationSign type={OperationSignType.subtraction} size={15} color={LinerColor.moonlitAsteroid} rotation={-135} zIndex={1} top={30} left={50} />
+            <OperationSign type={OperationSignType.subtraction} size={21} color={LinerColor.bloodyMary} rotation={-135} zIndex={1} top={30} left={70} />
           </section>
         </div>
       </section>
@@ -77,16 +77,16 @@ const HomePage: React.FunctionComponent = () => {
         <main>
           <header>Profile</header>
           <p>Introduction of Stan!</p>
-          <div id="goToProfile" className="goToProfileButton liner-color-yoda">Go</div>
+          <div id="goToProfile" className="goToProfileButton">GO</div>
         </main>
         <section className="operationSignGroup-4">
-          <OperationSign type={OperationSignType.addition} size={3} color={LinerColor['cool-sky']} rotation={90} zIndex={1} top={0} left={1.5} />
-          <OperationSign type={OperationSignType.addition} size={3} color={LinerColor['witching-hour']} rotation={45} zIndex={2} top={1} left={5} />
-          <OperationSign type={OperationSignType.subtraction} size={3} color={LinerColor['bloody-mary']} rotation={90} zIndex={3} top={4} left={4.5} />
-          <OperationSign type={OperationSignType.division} size={3} color={LinerColor['moonlit-asteroid']} rotation={135} zIndex={4} top={3} left={0} />
+          <OperationSign type={OperationSignType.addition} size={3} color={LinerColor.coolSky} rotation={90} zIndex={1} top={0} left={1.5} />
+          <OperationSign type={OperationSignType.addition} size={3} color={LinerColor.witchingHour} rotation={45} zIndex={2} top={1} left={5} />
+          <OperationSign type={OperationSignType.subtraction} size={3} color={LinerColor.bloodyMary} rotation={90} zIndex={3} top={4} left={4.5} />
+          <OperationSign type={OperationSignType.division} size={3} color={LinerColor.moonlitAsteroid} rotation={135} zIndex={4} top={3} left={0} />
         </section>
       </section>
-    </>
+    </Suspense>
   )
 }
 
