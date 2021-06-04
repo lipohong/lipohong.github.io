9cf607934bc3166cc2ff4d3639623b480d097216
lipohong <lipohong@hotmail.com>
2021-06-04 17:57:42 +0800

    Profile page work in progress

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -8,39 +8,49 @@ const ProfilePage: React.FunctionComponent = () => {
   return (
     <Suspense fallback={<main className="lazyLoading">loading...</main>}>
       <main className="profile">
-        <section className="firstSection">
-          <main>
-            <header>
-              <hr />
-              <span>Stan</span>
-              <span>Li</span>
-              <hr />
-            </header>
-            <section className="upperSection">
-              <a href="https://github.com/lipohong">
-                <Icon path={mdiGithub} size={1.5} />
-              </a>
-              <a href="https://www.linkedin.com/in/stan-li-245a5b182">
-                <Icon path={mdiLinkedin} size={1.5} />
-              </a>
-            </section>
-            <section className="lowerSection">
-              <header>Summary</header>
-              <hr />
-              <p>Stan Li is a front-end developer, with back-end development skills and experience as well.</p>
-            </section>
-          </main>
-          <img src={profile} title="profile" alt="profile"></img>
-        </section>
-        <section className="secondSection">
-          <header></header>
-          <p></p>
-          <footer>
-            <section></section>
-            <section></section>
-            <section></section>
-          </footer>
-        </section>
+        <div className='container'>
+          <section className="firstSection">
+            <main>
+              <header>
+                <hr />
+                <span>Stan</span>
+                <span>Li</span>
+                <hr />
+              </header>
+              <section className="upperSection">
+                <a href="https://github.com/lipohong">
+                  <Icon path={mdiGithub} size={1.5} />
+                </a>
+                <a href="https://www.linkedin.com/in/stan-li-245a5b182">
+                  <Icon path={mdiLinkedin} size={1.5} />
+                </a>
+              </section>
+              <section className="lowerSection">
+                <header>Summary</header>
+                <hr />
+                <p>A front-end developer, with back-end development skills and experience as well.</p>
+              </section>
+            </main>
+            <div className="profileContainer">
+              <img src={profile} title="profile" alt="profile"></img>
+            </div>
+          </section>
+          <section className="secondSection">
+            <header className="title">Work Experience</header>
+            <main>
+              <section>
+                
+              </section>
+              <section></section>
+            </main>
+          </section>
+          <section className="thirdSection">
+            <header className="title">Education</header>
+          </section>
+          <section className="forthSection">
+            <header className="title">Skills</header>
+          </section>
+        </div>
       </main>
     </Suspense>
   )
