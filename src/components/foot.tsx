fcffb3f4205f1bb0ed1f99d253413837b4358c06
lipohong <lipohong@hotmail.com>
2021-06-21 11:52:42 +0800

    Update footer layout

--- src/components/foot.tsx
+++ src/components/foot.tsx
@@ -7,36 +7,28 @@ const Foot: React.FunctionComponent = () => {
 
   return (
     <footer className="foot">
-      <div className="upperContainer">
-        <header>
-          <main>
-            Make <span>Excellent</span> Website and App
-          </main>
-          <footer>
-            iWebsite<sup>©</sup> 2021 by Stan Li
-          </footer>
-        </header>
+      <header className="header">
+        Make <span>Excellent</span> Website and App
+      </header>
+      <main className="contactInfo">
+        <header>CONTACT INFO</header>
         <main>
-          <header>CONTACT INFO</header>
-          <main>
-            <section><Icon path={mdiPhone} size={1} /> +852 9048 3591</section>
-            <section><Icon path={mdiEmail} size={1} /> lipohong@hotmail.com</section>
-          </main>
+          <section><Icon path={mdiPhone} size={1} /> +852 9048 3591</section>
+          <section><Icon path={mdiEmail} size={1} /> lipohong@hotmail.com</section>
         </main>
-      </div>
-      <footer>
-        <section>
-          <Link to='/profile'>About me</Link>
-        </section>
-        <section>
-          <a href="https://github.com/lipohong/lipohong.github.io">
-            <Icon path={mdiGithub} size={1} />
-          </a>
-          <a href="https://www.linkedin.com/in/stan-li-245a5b182">
-            <Icon path={mdiLinkedin} size={1} />
-          </a>
-        </section>
-      </footer>
+      </main>
+      <section className="copyRightInfo">iWebsite<sup>©</sup> 2021 by Stan Li</section>
+      <section className="aboutMe">
+        <Link to='/profile'>About me</Link>
+      </section>
+      <section className="socialLink">
+        <a href="https://github.com/lipohong/lipohong.github.io">
+          <Icon path={mdiGithub} size={1} />
+        </a>
+        <a href="https://www.linkedin.com/in/stan-li-245a5b182">
+          <Icon path={mdiLinkedin} size={1} />
+        </a>
+      </section>
     </footer>
   )
 }
